2c848a3363658f52ca838863c3a19088c80d7aca
marlonprado2025 <marlon123po2@gmail.com>
2025-03-03 02:07:51 -0300

    update result p

--- Java.js
+++ Java.js
@@ -2,7 +2,7 @@ function generateNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-    
+    const p = document.querySelector(".result-number")
 
 
     if (min >= max) {
@@ -12,8 +12,7 @@ function generateNumber() {
 
         const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  alert(result)
-
+        p.innerHTML = `${result}`;
      }
     
 } 
